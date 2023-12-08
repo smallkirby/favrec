5a08464c0ea1d9fb82b4a30c00f38c814e1cb4a8
smallkirby <ssmallkirby@gmail.com>
2023-12-09 03:17:37 +0900

    animate logo heart on hover
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -11,12 +11,15 @@ export default function Header() {
   return (
     <header className="navbar m-4 flex items-center justify-between">
       <button
-        className="flex cursor-pointer items-center border-none py-0 font-cute
-        text-pink-500 duration-300 hover:text-pink-800 dark:bg-slate-800"
+        className="group flex cursor-pointer items-center border-none py-0
+        font-cute text-pink-500 duration-300 hover:text-pink-800 dark:bg-slate-800"
         onClick={() => router.push('/')}
       >
         <h1 className="mr-1 text-lg">FAVREC</h1>
-        <FavoriteIcon sx={{ width: 20, height: 20 }} className="pb-[1px]" />
+        <FavoriteIcon
+          sx={{ width: 20, height: 20 }}
+          className="pb-[1px] group-hover:animate-ping"
+        />
       </button>
 
       <div className="ml-8 flex-1">
