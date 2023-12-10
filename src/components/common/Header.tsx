3ae75d1ba1984ca9b893f7c522d6eeb0efc57eec
smallkirby <ssmallkirby@gmail.com>
2023-12-10 17:18:35 +0900

    two row header for sp
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,24 +9,30 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <header className="navbar flex items-center justify-between px-4 py-2">
-      <button
-        className="group flex cursor-pointer items-center border-none py-0
+    <header className="navbar mb-4">
+      <div className="flex items-center justify-between px-4 pt-2">
+        <button
+          className="group flex cursor-pointer items-center border-none py-0
         font-cute text-pink-500 duration-300 hover:text-pink-800 dark:bg-slate-800"
-        onClick={() => router.push('/')}
-      >
-        <h1 className="mr-1 text-lg">FAVREC</h1>
-        <FavoriteIcon
-          sx={{ width: 20, height: 20 }}
-          className="pb-[1px] group-hover:animate-ping"
-        />
-      </button>
+          onClick={() => router.push('/')}
+        >
+          <h1 className="mr-1 text-lg">FAVREC</h1>
+          <FavoriteIcon
+            sx={{ width: 20, height: 20 }}
+            className="pb-[1px] group-hover:animate-ping"
+          />
+        </button>
 
-      <div className="ml-8 flex-1">
-        <HeadMenu />
+        <div className="ml-8 hidden flex-1 pr-4 md:block">
+          <HeadMenu />
+        </div>
+
+        <UserBadge />
       </div>
 
-      <UserBadge />
+      <div className="md:hidden">
+        <HeadMenu />
+      </div>
     </header>
   );
 }
