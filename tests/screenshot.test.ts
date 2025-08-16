28e95b515117119cf85fb1b6d58eed8fa02e116d
smallkirby <ssmallkirby@gmail.com>
2025-08-16 17:41:15 +0900

    test: minor fix
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- tests/screenshot.test.ts
+++ tests/screenshot.test.ts
@@ -3,13 +3,13 @@ import { expect, test } from '@playwright/test';
 test('Screenshot of index', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(page).toHaveScreenshot('index-screenshot.png', {
-    maxDiffPixels: 5000,
+    maxDiffPixels: 6500,
   });
 });
 
 test('Screenshot of settings page', async ({ page }) => {
   await page.goto('http://localhost:3000/settings');
   await expect(page).toHaveScreenshot('settings-screenshot.png', {
-    maxDiffPixels: 5000,
+    maxDiffPixels: 6500,
   });
 });
